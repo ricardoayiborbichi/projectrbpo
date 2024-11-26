26b454c0a75788f46aac79d0b284449a19222147
ricardoayiborbichi <ricardoayibor@gmail.com>
2024-11-27 00:54:21 +0300

    Initial commit

---
+++ src/main/java/ru/mtuci/projectrbpo/ProjectRbpoApplication.java
@@ -0,0 +1,13 @@
+package ru.mtuci.projectrbpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRbpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRbpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mtuci/projectrbpo/ProjectRbpoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.projectrbpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRbpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
