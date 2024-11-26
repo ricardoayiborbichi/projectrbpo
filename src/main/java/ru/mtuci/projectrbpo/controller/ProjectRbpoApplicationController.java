db6bdd00d4c704eb63ac80083683fc0ee6a905ac
ricardoayiborbichi <ricardoayibor@gmail.com>
2024-11-27 01:24:47 +0300

    2

--- src/main/java/ru/mtuci/projectrbpo/controller/ProjectRbpoApplicationController.java
+++ src/main/java/ru/mtuci/projectrbpo/controller/ProjectRbpoApplicationController.java
@@ -1,4 +1,13 @@
 package ru.mtuci.projectrbpo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProjectRbpoApplicationController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
 }
